734278f138b0cdf3afda76aa3da9b308bffd43c5
554878526@qq.com <554878526@qq.com>
2021-02-08 22:32:25 +0800

    firstTime commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>你好世界</h1>
+      <h1>wokankan</h1>
+      <h1>bucuoya</h1>
+      <h1>这个厉害了</h1>
+      <h1>这个真的厉害</h1>
+    </div>
+  )
+}
